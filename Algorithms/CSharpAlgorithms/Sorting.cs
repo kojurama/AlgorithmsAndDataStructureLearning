9a6598fd6ada4e834bce85714be7c89a38527fda
kojurama <kojurama@frontier.com>
2019-11-21 03:30:50 -0800

    Insertion sort and selection sort added with tests

--- Algorithms/CSharpAlgorithms/Sorting.cs
+++ Algorithms/CSharpAlgorithms/Sorting.cs
@@ -18,6 +18,37 @@ namespace CSharpAlgorithms
             }
         }
 
+        public static void SelectionSort(int[] array)
+        {
+            for (int partIndex = array.Length-1; partIndex > 0; partIndex--)
+            {
+                int largestAt = 0;
+                for (int i = 1; i <= partIndex; i++)
+                {
+                    if (array[i] > array[largestAt])
+                    {
+                        largestAt = i;
+                    }
+                }
+                Swap(array, largestAt, partIndex);
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for(int partIndex = 1; partIndex < array.Length; partIndex++)
+            {
+                int curUnsorted = array[partIndex];
+                int i = 0;
+                for (i = partIndex; i > 0 && array[i-1] > curUnsorted; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+
+                array[i] = curUnsorted;
+            }
+        }
+
         private static void Swap(int[] array, int i, int j)
         {
             if (i == j)
--- AlgorithmsDSLibrary/SortingTests.cs
+++ AlgorithmsDSLibrary/SortingTests.cs
@@ -14,7 +14,7 @@ namespace AlgorithmsDSLibrary
             samples[1] = new[] { 2, 1 };
             samples[2] = new[] { 2, 1, 3 };
             samples[3] = new[] { 1, 1, 1 };
-            samples[4] = new[] { 2,-1, 3, 3 };
+            samples[4] = new[] { 2, -1, 3, 3 };
             samples[5] = new[] { 4, -5, 3, 3 };
             samples[6] = new[] { 0, -5, 3, 3 };
             samples[7] = new[] { 0, -5, 3, 0 };
@@ -47,6 +47,18 @@ namespace AlgorithmsDSLibrary
         public void BubbleSort_ValidInput_SortedInput()
         {
             RunTestsForSortAlgorithm(Sorting.BubbleSort);
+        }
+
+        [Test]
+        public void SelectionSort_ValidInput_SortedOutput()
+        {
+            RunTestsForSortAlgorithm(Sorting.SelectionSort);
+        }
+
+        [Test]
+        public void InsertionSort_ValidInput_SortedOutput()
+        {
+            RunTestsForSortAlgorithm(Sorting.SelectionSort);
         }
     }
 }
