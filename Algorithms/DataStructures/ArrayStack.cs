c00fd9a48681aeed1edacc804da1d4e1c1b41d47
kojurama <kojurama@frontier.com>
2019-12-02 05:26:01 -0800

    Added an array stack with unit tests.

---
+++ Algorithms/DataStructures/ArrayStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayStack
+{
+    public class ArrayStack<T>
+    {
+
+        private T[] _items;
+        public ArrayStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public ArrayStack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public T Peek()
+        {
+            return _items[Count - 1];
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+
+            _items[--Count] = default(T);
+        }
+
+        public void Push(T item)
+        {
+            if (_items.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_items, largerArray, Count);
+
+                _items = largerArray;
+            }
+
+            _items[Count++] = item;
+        }
+
+        public bool IsEmpty => Count == 0;
+        public int Count { get; private set; }
+
+    }
+}
---
+++ Algorithms/DataStructures/ArrayStackTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ArrayStack;
+using NUnit.Framework;
+
+namespace DataStructures
+{
+    [TestFixture]
+    public class ArrayStackTests
+    {
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            var stack = new ArrayStack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Count_PushOneItem_ReturnsOne()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsException()
+        {
+            var stack = new ArrayStack<int>();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                stack.Pop();
+            });
+        }
+
+        [Test]
+        public void Peek_PushTwoItems_ReturnsHeadItem()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            Assert.AreEqual(2, stack.Peek());
+        }
+
+        [Test]
+        public void Peek_PushTwoItemsAndPop_ReturnsHeadElement()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Pop();
+
+            Assert.AreEqual(1, stack.Peek());
+        }
+    }
+}
